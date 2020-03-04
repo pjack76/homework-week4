7208cdcfaf5c8aa0532baae3b3be4e723355240f
Jack Long <putheara.long@gmail.com>
2020-03-03 20:43:23 -0500

    got buttons to click

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //my question list
 var qList = [
-    ["What is the capital Nebraska?", "Lincoln", "Omaha", "Sioux Falls", "Sioux City", "A"],
-    ["What is the capital Virginia?", "Arlington", "Fairfax", "Richmond", "Charlottesville", "C"],
-    ["What is the capital New York?", "New York City", "Albany", "Syracuse", "Buffalo", "B"],
-    ["What is the capital New Mexico?", "Albuquerque", "New Mexico City", "Roswell", "Santa Fe", "D"],
-    ["What is the capital Idaho?", "Potato City", "Boise", "Helena", "Portland", "B"]
+    ["What is the capital Nebraska?", "Lincoln", "Omaha", "Sioux Falls", "Sioux City", "Lincoln"],
+    ["What is the capital Virginia?", "Arlington", "Fairfax", "Richmond", "Charlottesville", "Richmond"],
+    ["What is the capital New York?", "New York City", "Albany", "Syracuse", "Buffalo", "Albany"],
+    ["What is the capital New Mexico?", "Albuquerque", "New Mexico City", "Roswell", "Santa Fe", "Santa Fe"],
+    ["What is the capital Idaho?", "Potato City", "Boise", "Helena", "Portland", "Boise"]
 ];
 // keep score
 var score = 0;
@@ -12,24 +12,25 @@ var score = 0;
 var qCounter = 0;
 // this function is for loading questions and options to html
 function qLoader() {
-    var showQhere = document.getElementById("#questions");
-    var option1 = document.getElementById("#choice1");
-    var option2 = document.getElementById("#choice2");
-    var option3 = document.getElementById("#choice3");
-    var option4 = document.getElementById("#choice4");
-    var qText, answer, ansOpt1, ansOpt2, ansOpt3, ansOpt4
-
+    var showQhere = document.getElementById("questions");
+    var option1 = document.getElementById("choice1");
+    var option2 = document.getElementById("choice2");
+    var option3 = document.getElementById("choice3");
+    var option4 = document.getElementById("choice4");
+    var qText, ansOpt1, ansOpt2, ansOpt3, ansOpt4
+    
+    
     qText = qList[qCounter][0];
     ansOpt1 = qList[qCounter][1];
     ansOpt2 = qList[qCounter][2];
     ansOpt3 = qList[qCounter][3];
     ansOpt4 = qList[qCounter][4];
 
-    showQhere.innerHTML += qText;
-    option1.textContent += ansOpt1;
-    option2.innerHTML += ansOpt2;
-    option3.innerHTML += ansOpt3;
-    option4.innerHTML += ansOpt4;
+    showQhere.innerHTML = qText;
+    option1.innerHTML = ansOpt1;
+    option2.innerHTML = ansOpt2;
+    option3.innerHTML = ansOpt3;
+    option4.innerHTML = ansOpt4;
 };
 
     window.addEventListener("load", qLoader);
@@ -37,14 +38,23 @@ function qLoader() {
 //this function is for comparing anwers
 
 function evalAns () {
+    var userAns, correctAns
+    var bttn = document.querySelector(".button-grp");
 
+    bttn.addEventListener("click", function() {
+        bttn.children.innerText = userAns;
+        correctAns = qList[qCounter][5];
+        if (userAns === correctAns) {
+            score++;
+            alert ("Your Score:" +" " +score + "\n" + "CORRECT!!")
+        } else alert ("WRONG!!");});
 
-
-
-
-qLoader();
+        qCounter++;
+    
+    qLoader();
 };
 
+evalAns();
 
 
 
